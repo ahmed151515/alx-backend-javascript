6e99701d66ba7ecbdfa2e21d47fb1f129c2ef7ea
ahmed arafa <ahmedarafa.200444@gmail.com>
2024-07-23 09:33:24 +0300

    .

--- 0x00-ES6_basic/test/0-constants.test.js
+++ 0x00-ES6_basic/test/0-constants.test.js
@@ -1,7 +1,7 @@
 import { taskFirst, taskNext } from "../0-constants.js";
 
-test("test", () => {
-  expect(`${taskFirst()} ${taskNext()}`).toBe(
+test("tasks are correctly defined", () => {
+  expect(`${taskFirst()} ${taskNext()}`).toEqual(
     "I prefer const when I can. But sometimes let is okay"
   );
 });
