1161f97d4c0cce58f80ae97a17c85eacc0ba8a14
ahmed arafa <ahmedarafa.200444@gmail.com>
2024-07-23 10:15:31 +0300

    .

--- 0x00-ES6_basic/test/5-spread-operator.test.js
+++ 0x00-ES6_basic/test/5-spread-operator.test.js
@@ -1,7 +1,7 @@
 import concatArrays from '../5-spread-operator';
 
 test('returns the right number of arguments', () => {
-  expect(concatArrays(['a', 'b'], ['c', 'd'], 'Hello')).toBe([
+  expect(concatArrays(['a', 'b'], ['c', 'd'], 'Hello')).toEqual([
     'a', 'b', 'c',
     'd', 'H', 'e',
     'l', 'l', 'o'
